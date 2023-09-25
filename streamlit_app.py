daf980821e7fc541c63218ec461b4d744945d881
Sven Trebard <trebard@gmail.com>
2023-09-25 17:29:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/" + this_fruit_choice)
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json)
-  return fruityvice normalized                                               
+  return fruityvice_normalized                                               
                                               
 streamlit.header("Fruityvice Fruit Advice!")
 
